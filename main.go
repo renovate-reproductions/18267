b129ff438cc2e407ab0597810b8e70c4abe90665
Enes Kayali <enes.kayali@sap.com>
2022-10-11 16:54:31 +0200

    add reproduction files

---
+++ main.go
@@ -0,0 +1,8 @@
+package renovate_issue_17885
+
+import (
+	"go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-sdk-go-v2/otelaws"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+)
